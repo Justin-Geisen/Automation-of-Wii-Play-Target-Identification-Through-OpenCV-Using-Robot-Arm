dd594072a251c46a1a0eac71595954395c682011
duy nguyen <duy.nguyen9@mavs.uta.edu>
2023-07-30 20:05:54 -0500

    add cursor finder

--- cv_background.py
+++ cv_background.py
@@ -5,8 +5,21 @@ import time
 import itertools as it
 
 
-def findCursor(frame, template):
-	pass
+def findCursorTM(frame, template):
+	grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	grayTemplate = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
+	templateW, templateH = grayTemplate.shape[::-1]
+	templateRes = cv.matchTemplate(grayFrame, grayTemplate, cv.TM_CCOEFF_NORMED)
+
+	_, maxVal, _, max_loc = cv.minMaxLoc(templateRes)
+	# print(maxVal)
+	if maxVal >= .7:
+		return True, (max_loc[0], max_loc[1], templateW, templateH)
+	else:
+		return False, (0,0,0,0)
+	# return foundLoc[0], foundLoc[1], templateW, templateH
+	# return pt, (pt[0] + templateW, pt[1] + templateH)
+
 
 def detectObj (frame):
 	roi_frame = frame[ 0:715, 0:1920 ]
@@ -57,18 +70,25 @@ def sortedShortestPath(detectedContours):
 	paths = [ p for p in it.permutations(detectedContours) ]
 	path_distances = [ sum(map(lambda x: dist(x[0],x[1]), zip(p[:-1],p[1:]))) for p in paths ]
 	min_index = np.argmin(path_distances)
-	print("----2----",paths[min_index])
 	return paths[min_index]
 
 # capture live footage
 # vid = cv.VideoCapture( 1 )
-vid = cv.VideoCapture('Video\game_footage.mkv')
+
+###### MAIN LOOP ######
+
+# vid = cv.VideoCapture('Video\game_footage.mkv')
+# vid = cv.VideoCapture('Video\game_footage_2.mkv')
+vid = cv.VideoCapture('Video\game_footage_3.mkv')
 template = cv.imread('cursor.png')
+
 # background subtraction parameter (change to get better detection)
 bgHistory = 110
 bgVarThreshold = 300
 bgDetectShadow = False
 obj_dectection = cv.createBackgroundSubtractorMOG2(bgHistory, bgVarThreshold, bgDetectShadow)
+
+tracker = cv.TrackerCSRT_create()
 
 # fps counter
 prev_frame_time = 0
@@ -82,36 +102,42 @@ while(True):
 	# Capture the video frameq
 	ret, frame = vid.read()
 
-	# findCursor
+	returnSuccc = False
+	# find the Cursor
 	if not isCursorFound:
-		# pass
-		cursorRec = findCursor(frame, template)
-		#init
-		#isCursorFound=True
+		cursorReturn, cursorRectangle = findCursorTM(frame, template)
+		if cursorReturn :
+			tracker.init(frame, cursorRectangle)
+			isCursorFound = True
 	else: 
-		pass
-		# update -> succ, bbox
+		returnSuccc, cursorBox = tracker.update(frame)
 
-	# if succ
-	# cursor = cursorPosition(bbox)
-	# draw marker on scene
+	if returnSuccc:
+		cBoxTL = (int(cursorBox[0]), int(cursorBox[1]))
+		cBoxBR = (int(cursorBox[0] + cursorBox[2]), int(cursorBox[1] + cursorBox[3]))
+		cBoxCenter = (int((cBoxTL[0] +cBoxBR[0])/2), int((cBoxTL[1] + cBoxBR[1])/2))
+		cv.rectangle(frame, cBoxTL, cBoxBR, (255,0,0), 2, 1)
+		cv.drawMarker(frame, (cBoxCenter[0], cBoxCenter[1]), (255,0,0))
+		cv.putText(frame, "Tracking cursor", (100,80), cv.FONT_HERSHEY_SIMPLEX, 0.75,(0,255,0),2)
+	else:
+		cv.putText(frame, "Tracking cursor failure", (100,80), cv.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
-	# This is the area that we are interested in using
-	detectedContours = detectObj(frame)
+	# TODO: broken code, need to find solutions
+	# if isCursorFound: 
+	# 	# This is the area that we are interested in using
+	# 	detectedContours = detectObj(frame)
 
-	# This sorts the list of detected contours
-	sortedDetectedContours = sortedShortestPath(detectedContours)
+	# 	# This sorts the list of detected contours
+	# 	sortedDetectedContours = sortedShortestPath(detectedContours)
 
-	# print(detectedContours)
-	drawLine(sortedDetectedContours, color=(255,255, 0))
-
+	# 	# print(detectedContours)
+	# 	drawLine(sortedDetectedContours, color=(255,255, 0))
 
 	# fps counter
 	new_frame_time = time.time()
 	fps = 1/(new_frame_time-prev_frame_time)
 	prev_frame_time = new_frame_time
 	fps = str(int(fps))
-	
 	cv.putText(frame, fps, (7, 70), cv.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 0, 255), 3, cv.LINE_AA)
 
 	# Display the resulting frame
